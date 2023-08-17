92e8cb9903babca4851a8e8d55b87f1ae13f4739
Sam Mariassouce <sammariassouce@gmail.com>
2023-08-16 15:55:03 -1000

    nav

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from '../components/PageNav';
+
 function PageNotFound() {
-  return <div>Page Not found</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Page Not found</h1>
+    </div>
+  );
 }
 
 export default PageNotFound;
